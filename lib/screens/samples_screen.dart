575f1322829cc4d1f725534920fb5f9e2c0f1e29
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2023-10-24 15:48:28 -0300

    popdown menu added

--- lib/screens/samples_screen.dart
+++ lib/screens/samples_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:labtracking/screens/login_screen.dart';
+import 'package:labtracking/utils/routes.dart';
 
 import '../services/auth_service.dart';
 
@@ -13,6 +15,8 @@ class SamplesScreen extends StatefulWidget {
 }
 
 class _SamplesScreenState extends State<SamplesScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
   bool isLoading = false;
 
   @override
@@ -37,6 +41,47 @@ class _SamplesScreenState extends State<SamplesScreen> {
           ],
         ),
         backgroundColor: const Color.fromARGB(255, 126, 217, 87),
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(
+              Icons.menu,
+              size: 30,
+            ),
+            itemBuilder: (context) {
+              return [
+                const PopupMenuItem<int>(
+                  value: 0,
+                  child: Text("My Account"),
+                ),
+                const PopupMenuItem<int>(
+                  value: 1,
+                  child: Text("Settings"),
+                ),
+                const PopupMenuItem(
+                  value: 2,
+                  child: Text('Add new sample type'),
+                ),
+                const PopupMenuItem<int>(
+                  value: 3,
+                  child: Text("Logout"),
+                ),
+              ];
+            },
+            onSelected: (value) {
+              if (value == 0) {
+                print("My account menu is selected.");
+              } else if (value == 1) {
+                print("Settings menu is selected.");
+              } else if (value == 2) {
+                print("New sample type menu is selected.");
+              } else if (value == 2) {
+                AuthService.logout(_auth, _googleSignIn);
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil(AppRoutes.HOME, (route) => false);
+              }
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Column(
