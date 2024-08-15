3bb69446620babb822d5f16681412bac6f0572c0
Pedro <pjf_fernandes@yahoo.com.br>
2024-08-15 15:30:30 -0300

    flowchart created (its not good)

--- lib/screens/track_screen.dart
+++ lib/screens/track_screen.dart
@@ -9,6 +9,7 @@ import 'package:labtracking/utils/routes.dart';
 import '../services/auth_service.dart';
 import 'package:labtracking/components/about_window.dart';
 import "../screens/sample_details.screen.dart";
+import '../utils/arrow_painter.dart';
 
 class TrackScreen extends StatefulWidget {
   final Sample sample;
@@ -20,75 +21,102 @@ class TrackScreen extends StatefulWidget {
 
 class _TrackScreenState extends State<TrackScreen> {
   Widget buildSampleTree(Sample sample, [int level = 0]) {
-    // Create a list of widgets for the current sample and its children
-    List<Widget> widgetList = [
+    List<Widget> widgetList = [];
+
+    // Add the current sample card
+    widgetList.add(
       Padding(
         padding: EdgeInsets.only(left: 30.0 * level),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 2.0, left: 5),
-          child: InkWell(
-            onTap: () {
-              Navigator.of(context)
-                  .pushNamed(AppRoutes.SAMPLE_DETAILS, arguments: sample);
-            },
-            child: Card(
-              color: sample.exists == true
-                  ? Color.fromARGB(255, 154, 241, 180)
-                  : Color.fromARGB(255, 236, 173, 173),
-              elevation: 5,
-              child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: SizedBox(
-                  width: 150,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Icon(
-                            sample.exists == true ? Icons.check : Icons.cancel,
-                            color: sample.exists == true
-                                ? Colors.lightBlue
-                                : Colors.red,
+        child: InkWell(
+          onTap: () {
+            Navigator.of(context)
+                .pushNamed(AppRoutes.SAMPLE_DETAILS, arguments: sample);
+          },
+          child: Card(
+            color: sample.exists == true
+                ? Color.fromARGB(255, 154, 241, 180)
+                : Color.fromARGB(255, 236, 173, 173),
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: SizedBox(
+                width: 150,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          sample.exists == true ? Icons.check : Icons.cancel,
+                          color: sample.exists == true
+                              ? Colors.lightBlue
+                              : Colors.red,
+                        ),
+                        Text(
+                          " ${sample.name!}",
+                          style: const TextStyle(
+                            color: Colors.black,
                           ),
-                          Text(
-                            " ${sample.name!}",
-                            style: const TextStyle(
-                              color: Colors.black,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Text(
-                        sample.date!,
+                        ),
+                      ],
+                    ),
+                    Text(
+                      sample.date!,
+                      style: const TextStyle(color: Colors.blueGrey),
+                    ),
+                    FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: Text(
+                        sample.researcherEmail!,
                         style: const TextStyle(color: Colors.blueGrey),
                       ),
-                      FittedBox(
-                        fit: BoxFit.fitWidth,
-                        child: Text(
-                          sample.researcherEmail!,
-                          style: const TextStyle(color: Colors.blueGrey),
-                        ),
-                      )
-                    ],
-                  ),
+                    )
+                  ],
                 ),
               ),
             ),
           ),
         ),
       ),
-    ];
+    );
 
-    // Add children widgets if samples is a List and contains Sample objects
+    // Add child samples and the connecting lines
     if (sample.samples!.isNotEmpty) {
-      for (var element in sample.samples!) {
-        widgetList.add(buildSampleTree(
-            element, element.level == null ? 1 : element.level!));
+      for (var i = 0; i < sample.samples!.length; i++) {
+        var element = sample.samples![i];
+
+        // Add the connecting lines
+        widgetList.add(
+          Stack(
+            children: [
+              Positioned(
+                left: 30.0 * level +
+                    55, // Adjust horizontal alignment for vertical line
+                top: -2, // Adjust vertical position for vertical line
+                child: CustomPaint(
+                  painter: ArrowPainter(
+                    start: Offset(-30,
+                        -5.0 * level), // Starting point for the vertical line
+                    end: Offset(0, 50.0), // Length of the vertical line
+                    horizontalOffset: 30.0 *
+                        (level + 2), // Horizontal line extension to child
+                  ),
+                  size: const Size(
+                      50, 40), // Adjust size to cover the drawing area
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: 5.0 * (level + 1),
+                    top: 5), // Align the child card under the lines
+                child: buildSampleTree(element, level + 1),
+              ),
+            ],
+          ),
+        );
       }
     }
 
-    // Return a Column to display all widgets vertically
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: widgetList,
