a869da325be300fe2c2c9f6236c2bf8dfec06a8a
Pedro <pjf_fernandes@yahoo.com.br>
2024-10-30 13:22:22 -0300

    labs item list tile hover color added

--- lib/components/lab_item.dart
+++ lib/components/lab_item.dart
@@ -5,62 +5,70 @@ import 'package:labtracking/screens/samples_screen.dart';
 
 import 'package:labtracking/utils/routes.dart';
 
-class LabItem extends StatelessWidget {
+class LabItem extends StatefulWidget {
   final String labName;
   final String leaderName;
   final String id;
   final List<dynamic> members;
-  const LabItem(
-      {required this.id,
-      required this.labName,
-      required this.leaderName,
-      required this.members,
-      super.key});
+
+  const LabItem({
+    required this.id,
+    required this.labName,
+    required this.leaderName,
+    required this.members,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<LabItem> createState() => _LabItemState();
+}
+
+class _LabItemState extends State<LabItem> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  bool _isHovered = false;
+
+  void _labDetails() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => SamplesScreen(
+          labId: widget.id,
+          labName: widget.labName,
+          members: widget.members,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    final FirebaseAuth _auth = FirebaseAuth.instance;
-    final GoogleSignIn _googleSignIn = GoogleSignIn();
-    User? user;
-    void labDetails() {
-      // Navigator.of(context).pushNamed(AppRoutes.SAMPLES, arguments: {
-      //   'user': user,
-      //   'auth': _auth,
-      //   'google': _googleSignIn,
-      //   'labName': labName,
-      //   'labId': id,
-      // });
-      Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (ctx) => SamplesScreen(
-            labId: id,
-            labName: labName,
-            members: members,
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
+      child: Container(
+        color: _isHovered ? Colors.blue.withOpacity(0.1) : Colors.transparent,
+        child: ListTile(
+          trailing: ElevatedButton(
+            onPressed: _labDetails,
+            child: const Text(
+              "Samples",
+              style: TextStyle(color: Colors.white),
+            ),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.lightBlue,
+            ),
+          ),
+          title: Text(widget.labName, style: const TextStyle()),
+          subtitle: Text(
+            "Created by ${widget.leaderName}",
+            style: TextStyle(color: Colors.black54, fontSize: 10),
+          ),
+          leading: const CircleAvatar(
+            backgroundColor: Color.fromARGB(255, 241, 244, 246),
+            child: Icon(Icons.business, color: Colors.lightBlue),
           ),
         ),
-      );
-    }
-
-    return ListTile(
-      trailing: ElevatedButton(
-        onPressed: labDetails,
-        child: const Text(
-          "Samples",
-          style: TextStyle(color: Colors.white),
-        ),
-        style: ElevatedButton.styleFrom(
-            backgroundColor:
-                Colors.lightBlue //Color.fromARGB(255, 126, 217, 87),
-            ),
-      ),
-      title: Text(labName, style: const TextStyle()),
-      subtitle: Text(
-        "Created by $leaderName",
-        style: TextStyle(color: Colors.black54, fontSize: 10),
-      ),
-      leading: const CircleAvatar(
-        backgroundColor: Color.fromARGB(255, 241, 244, 246),
-        child: Icon(Icons.business, color: Colors.lightBlue),
       ),
     );
   }
